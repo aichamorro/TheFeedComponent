21de09044038d004382f9796d86b9d6c44d84b92
Alberto Chamorro <alberto.chamorro@hudl.com>
2016-11-17 18:14:31 +0000

    Adds more styling to the cells

--- TheFeedComponent/Classes/FeedUIViewController.swift
+++ TheFeedComponent/Classes/FeedUIViewController.swift
@@ -8,13 +8,19 @@
 
 import Foundation
 
+extension Int {
+    func isEven() -> Bool {
+        return self % 2 == 0
+    }
+}
+
 class FeedUIViewController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
